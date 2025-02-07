0beab486e1ebc15e6d730b1c996fa80b255eb2d3
TravisMa07 <travis07ma@gmail.com>
2025-02-06 19:31:35 -0500

    Homework 4 (version 1 submission)
    Submission requirements for Homework 4. Hopefully this is the only version I need to submit.

--- src/my-card.js
+++ src/my-card.js
@@ -14,7 +14,9 @@ export class MyCard extends LitElement {
 
   constructor() {
     super();
-    this.title = "My card";
+    this.title = "The Goat Lebron James";
+    this.image = 'https://people.com/thmb/zWm7qonVB_6R3JTGC583rNoiQWY=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():focal(719x209:721x211):format(webp)/lebron-james-bbd6f753bf124dfcb4d7e6b3284970f3.jpg';
+    this.desc = 'Default Description';
   }
 
   static get styles() {
@@ -22,16 +24,58 @@ export class MyCard extends LitElement {
       :host {
         display: block;
       }
+      .btn{
+  display: none;
+  background-color: #4A4A4A;
+  color: white;
+  font-size: 20px;
+  border-radius: 15%;
+  padding: 8px 8px 8px 8px;
+  margin: 4px 4px 4px 4px;
+}
+.card-item{
+  display: inine flex;
+  background-color:#FFFF00;
+  max-width: 400px;
+  padding: 8px;
+  margin: 8px;
+  border: 8px solid black;
+}
+
+.card-image{
+  max-width: 300px;
+  max-height: 250px;
+}
+
+.change-color{
+  background-color: #2E5A88;
+}
+
+.card-title{
+  font-size: 26px;
+}
+
     `;
   }
+  
 
   render() {
-    return html`<div>${this.title}</div>`;
+    return html`
+        <div class="card-item">
+        <img src="${this.image}" alt="${this.title}" class="card-image" />
+        <div>
+          <h2 class="card-title">${this.title}</h2>
+          <p>${this.desc}</p>
+        </div>
+      </div>
+      `;
   }
 
   static get properties() {
     return {
       title: { type: String },
+      link: { type: String},
+      desc: {type: String},
     };
   }
 }
